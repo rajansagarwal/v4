e8dfc8c7ff2551906d26afe0875c1d13b0f4b9b0
Rajan Agarwal <rajan.ag005@gmail.com>
2022-04-15 10:20:25 -0400

    fix styling

--- pages/[id].js
+++ pages/[id].js
@@ -10,31 +10,36 @@ export default function Post({ post }) {
   }
   return (
     <div>
-        <Head>
-        <title>{post.title}</title>
-        <meta name='description' content={post.subtitle} />
-      </Head>
-  <div className="two-pane">
-    <div id="main-content">
-      <div className="md:pl-[8vmin] md:pr-[7vmin] pl-[3vmin] pr-[3vmin]">
-        <h1 className="text-4xl text-black mt-4 font-bold font-mono tracking-wide">{post.title}</h1>
-        <h1 className="text-xl text-black mt-4 font-bold font-mono tracking-wide">{post.type} | {post.subtitle}</h1>
-        <p className="text-md text-black font-mono font-light my-4">{post.date}</p><hr/>
-        <div className="mt-8">
-          <ReactMarkdown className='font-serif lg:prose-lg prose-sm'>
-          {post.content}
-          </ReactMarkdown>
-      </div>
-        <br/><hr/><br/><br/><br/>
-      </div>
-    </div>
-    <div id="context-pane">
-      <div id="contexts">
-      <h1 className="text-xl text-black mt-4 font-bold font-mono tracking-wide">{post.context}</h1>
-      <p className="text-md text-black font-mono font-light my-4">{post.sidebar}</p><hr/>
-      </div>
-    </div>
+    <Head>
+    <title>{post.title}</title>
+    <meta name='description' content={post.subtitle} />
+  </Head>
+<div className="two-pane">
+<div id="main-content">
+  <div className="md:pl-[8vmin] md:pr-[5vmin] pl-[3vmin] pr-[1vmin]" id='main'>
+    <h1 className="text-4xl text-black mt-4 font-bold font-mono tracking-wide">{post.title}</h1>
+    <h1 className="text-xl text-black mt-4 font-bold font-mono tracking-wide">{post.type} | {post.subtitle}</h1>
+    <p className="text-md text-black font-mono font-light my-4">{post.date}</p><hr/>
+    <div className="mt-8">
+      <ReactMarkdown className='font-serif prose max-w-[100vw] prose-zinc prose-img:rounded-xl text-lg tracking-wide'>
+      {post.content}
+      </ReactMarkdown>
   </div>
+    <br/><hr/><br/><br/><br/>
+  </div>
+</div>
+<div id="context-pane">
+  <div id="contexts">
+  <h1 className="text-xl text-black mt-4 font-bold font-mono tracking-wide">{post.context}</h1>
+  <div className='my-8'>
+      <ReactMarkdown className='font-serif prose text-md'>
+      {post.sidebar}
+      </ReactMarkdown></div>
+      <hr/>
+      
+</div>
+</div>
+</div>
 </div>
   )
 }
