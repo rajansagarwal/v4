ce83a4382ac2797ca5e938301329c6bada03d317
Rajan Agarwal <rajan.ag005@gmail.com>
2022-04-15 22:45:01 -0400

    change to 65% margins

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,13 +1,17 @@
 import { Auth, Typography, Button } from "@supabase/ui";
 const { Text } = Typography
 import { supabase } from '../../api'
+import { useState, useEffect } from 'react'
 
 function Profile(props) {
     const { user } = Auth.useUser();
+    const [name, setName] = useState(null)
+
     if (user)
       return (
         <div className='p-[15vmin]'>
-          <Text>Signed in: {user.email}</Text>
+          <Text>Signed in: {user.email}</Text><br/>
+          
           <Button block onClick={() => props.supabaseClient.auth.signOut()}>
             Sign out
           </Button>
